d867def6a1fc01dcde10b4713cc4dda1858c354a
Andrew <a.kr.andreev@gmail.com>
2019-04-07 17:27:02 +0300

    initial commit with project initial skeleton

---
+++ isupport/src/main/java/org/isiktir/isupport/IsupportApplication.java
@@ -0,0 +1,13 @@
+package org.isiktir.isupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsupportApplication.class, args);
+    }
+
+}
